7b23ddcb7d36173f110fa4c53156c215fa58817f
0321ben <bochoamar0001@mymail.lausd.net>
2025-06-05 10:09:35 -0700

    rrrr

--- src/main/java/com/example/FinchSquare.java
+++ src/main/java/com/example/FinchSquare.java
@@ -4,10 +4,19 @@ import com.birdbrain.Finch;
 
 public class FinchSquare{
     public static void main(String args[]){
-        Finch myFinch = new Finch;
+        Finch myFinch = new Finch();
         myFinch.setMove("F", 10, 70);
-        myFinch.setTurn("R", 90, 70;
+        myFinch.setTurn("R", 90, 70);
         
+         myFinch.setMove("F", 10, 70);
+        myFinch.setTurn("R", 90, 70);
+
+         myFinch.setMove("F", 10, 70);
+        myFinch.setTurn("R", 90, 70);
+
+         myFinch.setMove("F", 10, 70);
+        myFinch.setTurn("R", 90, 70);
     }
+
 
 }
