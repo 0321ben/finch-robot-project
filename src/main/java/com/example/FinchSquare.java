37a33d9ad8005f7c810671baf99186c35ac3eab1
0321ben <bochoamar0001@mymail.lausd.net>
2025-06-05 09:22:09 -0700

    csdcds

---
+++ src/main/java/com/example/FinchSquare.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import com.birdbrain.Finch;
+
+public class FinchSquare{
+    public static void main(String args[]){
+        Finch myFinch = new Finch;
+        myFinch.setMove("F", 10, 70);
+        myFinch.setTurn("R", 90, 70;
+        
+    }
+
+}
